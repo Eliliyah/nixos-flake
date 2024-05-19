90e36d64a26936f9559754988064a32d0135af59
Jermeiah S <owner@arouzing.xyz>
2024-05-19 15:41:34 -0400

    formating

--- flake.nix
+++ flake.nix
@@ -1,33 +1,34 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    };
-    outputs = inputs: with inputs; let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+  outputs = inputs:
+    with inputs; let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
-        nixosConfigurations =  {
-            nixos =  nixpkgs.lib.nixosSystem {
-                # config._module.args = {inherit inputs;};
-                inherit system;
-                modules = [
-                    {
-                        nixpkgs.config.allowUnfree = true;
-                    }
-                    ./configuration.nix
-                ];
-            };
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          # config._module.args = {inherit inputs;};
+          inherit system;
+          modules = [
+            {
+              nixpkgs.config.allowUnfree = true;
+            }
+            ./configuration.nix
+          ];
         };
-        checks."${system}".default =  pkgs.nixosTest {
-            name = "minmal-test";
-            nodes.machine = ./configuration.nix;
+      };
+      checks."${system}".default = pkgs.nixosTest {
+        name = "minmal-test";
+        nodes.machine = ./configuration.nix;
 
-            testScript = ''
-                machine.wait_for_unit("default.target")
-            '';
-        } ;
+        testScript = ''
+          machine.wait_for_unit("default.target")
+        '';
+      };
     };
 }
