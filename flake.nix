46dae65b45e2fa88e96856e817dddd1f4dba2568
Jeremiah S <owner@arouzing.xyz>
2024-05-19 15:00:05 -0400

    update flake to stable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/c9b9378674d6c75c5c190da56e66a04bdc80258b";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     };
     outputs = inputs: with inputs; let
         system = "x86_64-linux";
