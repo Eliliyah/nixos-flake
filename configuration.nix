897879894376d05c73a797b9dae5cf4126cb18b3
Eliliyah <ellie.tiedemann@protonmail.com>
2024-05-19 14:20:04 -0400

    inital commit

---
+++ configuration.nix
@@ -0,0 +1,264 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+let
+  # nix-software-center = import (pkgs.fetchFromGitHub {
+  #   owner = "vlinkz";
+  #   repo = "nix-software-center";
+  #   rev = "0.1.2";
+  #   sha256 = "xiqF1mP8wFubdsAQ1BmfjzCgOD3YZf7EGWl9i69FTls=";
+  # }) {};
+in
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # ./modules/tuigreet.nix
+    ];
+  # enable flakes support
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+# Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Tell Xorg to use the nvidia driver (also valid for Wayland)
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is needed for most Wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = false;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.kernelParams = ["nouveau.modeset=0"];
+  boot.loader.grub.configurationLimit = 5;
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+services.hardware.openrgb = {
+package = pkgs.openrgb-with-all-plugins;
+enable = true;
+motherboard = "amd";
+};
+
+services.fstrim = {
+    enable = true;
+    interval = "weekly";
+  };
+
+services.asusd = {
+  enable = true;
+  enableUserService = true;
+};
+
+services.mullvad-vpn.enable = true;
+
+programs.rog-control-center.enable = true;
+
+programs.starship.enable = true;
+
+programs.fish.enable = true;
+
+programs.noisetorch.enable = true;
+
+services.flatpak.enable = true;
+
+services.blueman.enable = true;
+
+zramSwap.enable = true;
+
+programs.openvpn3.enable = true;
+
+hardware.bluetooth.enable = true;
+
+systemd.services.NetworkManager-wait-online.enable = false;
+
+services.supergfxd = {
+        enable = true;
+        settings = {
+          always_reboot = false;
+          no_logind = true;
+          mode = "Integrated";
+          vfio_enable = false;
+          vfio_save = false;
+          logout_timeout_s = 180;
+          hotplug_type = "None";
+        };
+      };
+
+# personal.desktop.displayManager.tuigreet.enable = true;
+
+nix.gc = {
+  automatic = true;
+  dates = "weekly";
+  options = "--delete-older-than 7d";
+};
+
+nix.optimise.automatic = true;
+
+virtualisation.virtualbox.host.enable = true;
+users.extraGroups.vboxusers.members = [ "ellie" ];
+virtualisation.virtualbox.host.enableExtensionPack = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ellie = {
+    isNormalUser = true;
+    description = "ellie";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      kate
+    #  thunderbird
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "ellie";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [ 
+latte-dock  
+vivaldi
+vivaldi-ffmpeg-codecs
+vscodium
+clementine
+gimp
+libreoffice-fresh
+neofetch
+discord
+plasma-browser-integration
+wineWowPackages.staging
+winetricks
+dosbox-staging
+ferdium
+appimage-run
+mullvad-vpn
+starship
+libsForQt5.discover
+# nix-software-center
+libsForQt5.ktorrent
+kcalc
+lutris
+libsForQt5.kamoso
+blueman
+bluez
+vlc
+
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+}
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/c9b9378674d6c75c5c190da56e66a04bdc80258b";
+    };
+    outputs = inputs: with inputs; let
+        system = "x86_64-linux";
+    in {
+        nixosConfigurations =  {
+            nixos =  nixpkgs.lib.nixosSystem {
+                # config._module.args = {inherit inputs;};
+                inherit system;
+                modules = [
+                    ./configuration.nix
+                ];
+            };
+        };
+    };
+}
