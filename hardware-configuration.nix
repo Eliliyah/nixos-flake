2eaa23f9bc55ed3717a24371d017837dc6294f0b
Jeremiah S <owner@arouzing.xyz>
2024-09-06 17:26:08 -0400

    updates changes for latest version (#2)
    * update to 24.05
    
    * remove zen kernel
    
    * fix system for migration to 24.05
    
    * update git ignore

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -7,16 +7,22 @@
   pkgs,
   modulesPath,
   ...
-}: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+}:
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "asus-ec-sensors"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = with config.boot.kernelPackages; [asus-ec-sensors];
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+    "asus-ec-sensors"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ asus-ec-sensors ];
+  # boot.kernelPackages = pkgs.linuxPackages_zen;
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/3cc73c9c-6485-472d-8556-395b40759130";
@@ -28,7 +34,7 @@
     fsType = "vfat";
   };
 
-  swapDevices = [];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
